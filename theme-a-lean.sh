fdc04568da66b2a6c42b3f9ad27e960561c72a63
fyfh <13757260730@sina.cn>
2020-12-06 19:35:28 +0800

    first
    update

---
+++ theme-a-lean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf package/lean/luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+./scripts/feeds update -a
+./scripts/feeds install -a
