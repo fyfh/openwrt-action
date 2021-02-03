0215dabfcccba4e274d41082f7cdc649cb2a1bf5
fyfh <13757260730@sina.cn>
2021-02-03 20:38:53 +0800

    Update diy-x86-64.sh

--- diy-x86-64.sh
+++ diy-x86-64.sh
@@ -10,5 +10,3 @@ sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_genera
 echo "src-git jerryk https://github.com/jerrykuku/openwrt-package
 src-git kenzo https://github.com/kenzok8/openwrt-packages
 src-git small https://github.com/kenzok8/small" >> feeds.conf.default
-./scripts/feeds update -a
-./scripts/feeds install -a
