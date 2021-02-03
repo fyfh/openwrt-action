adbe9e65c15f8a1a91e8b7716644b63d6afe23fb
fyfh <13757260730@sina.cn>
2021-02-03 21:44:47 +0800

    Update diy-x86-64.sh

--- diy-x86-64.sh
+++ diy-x86-64.sh
@@ -7,6 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
-git clone --depth=1 https://github.com/jerrykuku/openwrt-package package/one
-git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/two
-git clone --depth=1 https://github.com/kenzok8/small package/small
+git clone https://github.com/jerrykuku/openwrt-package package/one
+git clone https://github.com/kenzok8/openwrt-packages package/two
+git clone https://github.com/kenzok8/small package/small
