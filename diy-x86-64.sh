9344a0f4c3510dce74abccf684204fd995dd936e
fyfh <13757260730@sina.cn>
2021-02-03 21:25:21 +0800

    Update diy-x86-64.sh

--- diy-x86-64.sh
+++ diy-x86-64.sh
@@ -9,4 +9,4 @@
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 git clone --depth=1 https://github.com/jerrykuku/openwrt-package 
 git clone --depth=1 https://github.com/kenzok8/openwrt-packages
-git clone --depth=1 https://github.com/kenzok8/small.git" >> feeds.conf.default
+git clone --depth=1 https://github.com/kenzok8/small.git
