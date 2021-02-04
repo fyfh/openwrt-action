2ecf3e758c866454c38294df7942755b627f3aa4
fyfh <13757260730@sina.cn>
2021-02-04 17:23:32 +0800

    Update diy-x86-64.sh

--- diy-x86-64.sh
+++ diy-x86-64.sh
@@ -18,7 +18,8 @@ popd
 
 mkdir package/community
 pushd package/community
-
+# Add Lienol's Packages
+git clone --depth=1 https://github.com/Lienol/openwrt-package
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
