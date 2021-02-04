2a78f0f3d75db75da3e376c8f1a2d9abb355014d
fyfh <13757260730@sina.cn>
2021-02-04 13:36:07 +0800

    Update diy-x86-64.sh

--- diy-x86-64.sh
+++ diy-x86-64.sh
@@ -8,6 +8,54 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 #git clone https://github.com/jerrykuku/openwrt-package package/one
-git clone https://github.com/kenzok8/openwrt-packages package/two
-git clone https://github.com/kenzok8/small package/small
+#git clone https://github.com/kenzok8/openwrt-packages package/two
+#git clone https://github.com/kenzok8/small package/small
 #git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
+# Add luci-app-ssr-plus
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+popd
+
+mkdir package/community
+pushd package/community
+
+# Add luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+
+git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
+
+# Add luci-app-vssr <M>
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
+git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
+
+
+# Add smartdns
+svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
+
+# Add OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+
+# Add clash
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-clash ../luci-app-clash
+
+# Add aliddns
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-aliddns ../luci-app-aliddns
+
+# Add theme
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-argon_new ../luci-theme-argon_new
+
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial ../luci-theme-atmaterial
+
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge ../luci-theme-edge
+
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-ifit ../luci-theme-ifit
+
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentomato ../luci-theme-opentomato 
+
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentomcat ../luci-theme-opentomcat
+
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentopd ../luci-theme-opentopd
+
+
+popd
