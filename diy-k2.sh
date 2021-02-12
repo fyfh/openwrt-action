bdcef1c43ebf38b9bedd3c52b0696121e0ad3ab8
fyfh <13757260730@sina.cn>
2021-02-12 15:59:03 +0800

    Update diy-k2.sh

--- diy-k2.sh
+++ diy-k2.sh
@@ -7,3 +7,4 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
