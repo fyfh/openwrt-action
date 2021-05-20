136b8aa9ca02af6fc5ea8882bbf7422bdb316fc7
fyfh <13757260730@sina.cn>
2021-05-20 15:53:38 +0800

    Update diy-lineol.sh

--- diy-lineol.sh
+++ diy-lineol.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-mkdir openwrt/package/system/fstools/patches
-cd openwrt/package/system/fstools/patches && wget https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/system/fstools/patches/0001-fstools-support-extroot-for-non-MTD-rootfs_data.patch
-cd openwrt/package/system/fstools/ && rm Makefile && wget https://raw.githubusercontent.com/coolsnowwolf/lede/2f8dd6a67175ad55cc732b2943d880e61ff0849b/package/system/fstools/Makefile
+mkdir package/system/fstools/patches
+cd package/system/fstools/patches && wget https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/system/fstools/patches/0001-fstools-support-extroot-for-non-MTD-rootfs_data.patch
+cd package/system/fstools/ && rm Makefile && wget https://raw.githubusercontent.com/coolsnowwolf/lede/2f8dd6a67175ad55cc732b2943d880e61ff0849b/package/system/fstools/Makefile
