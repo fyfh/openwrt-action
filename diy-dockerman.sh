07dc9bffcabf8de995634dffe359d4af06fdca6b
sheno-Tyes <42202881+sheno-Tyes@users.noreply.github.com>
2021-11-25 23:26:10 +0800

    Create diy-dockerman.sh

---
+++ diy-dockerman.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman' >> feeds.conf.default
