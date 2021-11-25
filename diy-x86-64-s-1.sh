f88f5f00d75175260c21d1b64806838b13c3c484
sheno-Tyes <42202881+sheno-Tyes@users.noreply.github.com>
2021-11-25 12:23:46 +0800

    Update diy-x86-64-s-1.sh

--- diy-x86-64-s-1.sh
+++ diy-x86-64-s-1.sh
@@ -43,10 +43,8 @@ svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser ../l
 # ADD luci-app-syncthing
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-syncthing ../luci-app-syncthing
 # add docker
-rm -rf ../lean/luci-app-docker
-rm -rf ../lean/luci-lib-docker
 git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
-git clone --depth=1 https://github.com/lisaac/luci-lib-docker
+
 # Add luci-app-vssr <M>
 #git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
 #git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
