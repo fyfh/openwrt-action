a5e908bb3860f78a071005fdc958bb524fcde595
fyfh <13757260730@sina.cn>
2022-01-27 01:42:48 +0800

    Update diy-k2.sh

--- diy-k2.sh
+++ diy-k2.sh
@@ -8,5 +8,3 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '/routing/d' feeds.conf.default
-echo "src-git routing https://git.openwrt.org/feed/routing.git^a7b79a1d4fce4f443854115ee4b2ed2aafba5c06" >> feeds.conf.default
