24ce9a79c3aeab39646d86f0721c23591099c65b
fyfh <13757260730@sina.cn>
2022-02-09 21:39:44 +0800

    Update diy-hc5962.sh

--- diy-hc5962.sh
+++ diy-hc5962.sh
@@ -7,3 +7,4 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
